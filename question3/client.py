02c38f1bc89b918675e4e2f58b150d6fe39cbba7
Lucas Marchel <lucasmarchel13@gmail.com>
2019-06-23 01:22:01 -0300

    feat: improved client

--- question3/client.py
+++ question3/client.py
@@ -1,21 +1,23 @@
 import socket
 import os
+from threading import Thread
+import pickle
 
-class TCPClient:
+class TCPClient(Thread):
     def __init__(self, host='127.0.0.1', port=5000):
+        super(TCPClient, self).__init__()
         self.host = host
-        self.port = int(port)
+        self.port = int(port)        
+
+    def run(self):
         self.tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         destination = (self.host, self.port)
         self.tcp.connect(destination)
-        print 'Press CTRL+X\n to exit...'
+        print('Client connected to {}:{}'.format(self.host, self.port))
+        raw_input()
 
-        # msg = raw_input()
-        # while msg <> '\x18':
-        #     tcp.send (msg)
-        #     msg = raw_input()
-        # tcp.close()
+    def closeConnection(self):
+        self.tcp.close()
 
-
-if __name__ == "__main__":
-    client = TCPClient(port=os.environ.get('PORT'))
+    def sendData(self, data):   
+        self.tcp.send(pickle.dumps(data))
